ec4f03c2775de25032789d8c23c65cb4cb1f570c
tew404 <thammarat.tew@gmail.com>
2024-07-28 19:52:56 +0700

    Update

--- anykernel.sh
+++ anykernel.sh
@@ -7,9 +7,9 @@ properties() { '
 kernel.string=
 do.devicecheck=1
 do.modules=0
-do.systemless=0
+do.systemless=1
 do.cleanup=1
-do.cleanuponabort=1
+do.cleanuponabort=0
 device.name1=lisa
 device.name2=
 device.name3=
@@ -18,22 +18,6 @@ device.name5=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
-
-# shell variables
-block=boot;
-is_slot_device=1;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
-
-
-## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
-. tools/ak3-core.sh;
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 # Optimize F2FS extension list (@arter97)
 if mountpoint -q /data; then
@@ -77,6 +61,23 @@ if mountpoint -q /data; then
     done
   done
 fi
+
+
+# shell variables
+block=boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 ## AnyKernel boot install
 dump_boot;
